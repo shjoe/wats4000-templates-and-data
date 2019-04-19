6233dd4e3ca2ea5321321944b33792a6a4785da4
shjoe <shure.joe33@gmail.com>
2019-04-18 17:50:58 -0700

    Add docs from build

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,57 +1,62 @@
 <template>
   <div class="results">
-    <h1>Top Movies from the 20
+    <h1>
+      Top Movies from the 20
       <sup>th</sup> Century
     </h1>
     <p class="search-meta">
       <span class="current-page">
-        <b>Current Page:</b> 1
+        <b>Current Page:</b>
+        {{page}}
       </span>
-      <!-- TODO: Fill in current page value from data. -->
+
       <span class="total-pages">
-        <b>Pages:</b> 777
+        <b>Pages:</b>
+        {{total_pages}}
       </span>
-      <!-- TODO: Fill in total pages value from data. -->
+
       <span class="total-results">
-        <b>Count:</b> 3232
+        <b>Count:</b>
+        {{total_results}}
       </span>
-      <!-- TODO: Fill in results count value from data. -->
     </p>
 
     <ul>
-      <li class="movie-item">
-        <!-- TODO: Use a for loop to iterate through each result in the results array. -->
+      <li class="movie-item" v-for="(result, id) in results" :key="id">
         <img
-          src="https://image.tmdb.org/t/p/w150_and_h225_bestv2/p64TtbZGCElxQHpAMWmDHkWJlH2.jpg"
-          alt="Title of Movie Poster"
+          :src="'https://image.tmdb.org/t/p/w150_and_h225_bestv2/' + result.poster_path"
+          :alt="result.original_title"
           class="poster-image"
         >
-        <!-- TODO: Combine base URL with poster_path value in data to make the image src URL (Hint: Use v-bind.). -->
+
         <h2 class="title">
-          <a href="https://www.themoviedb.org/movie/78">Movie Title Goes Here</a>
+          <a :href="'https://www.themoviedb.org/movie/' + result.id">{{result.original_title}}</a>
         </h2>
-        <!-- TODO: Combine base URL with movie ID value in data to make the link href URL (Hint: Use v-bind.). -->
+
         <div class="ratings">
-          <span class="rating-category critics-choice">Critic's Choice</span>
-          <!-- TODO: Use a conditional to determine if the vote_average is over 8. -->
-          <span class="rating-category well-liked">Well Liked</span>
-          <!-- TODO: Use a conditional to determine if the vote_average is between 7 and 8. -->
-          <span class="rating-category stinker">Stinker</span>
-          <!-- TODO: Use a conditional to determine if the vote_average is under 7. -->
-          <span class="vote-average">8.4</span> with
-          <span class="vote-count">3676</span> votes
-          <!-- TODO: Fill in the vote_average and vote_count values accordingly. -->
+          <span
+            class="rating-category critics-choice"
+            v-if="result.vote_average > 8"
+          >Critic's Choice</span>
+
+          <span
+            class="rating-category well-liked"
+            v-if="result.vote_average > 7 && result.vote_average <= 8"
+          >Well Liked</span>
+
+          <span class="rating-category stinker" v-if="result.vote_average < 7">Stinker</span>
+
+          <span class="vote-average">{{result.vote_average}}</span> with
+          <span class="vote-count">{{result.vote_count}}</span>
+ votes
         </div>
-        <p class="overview">
-          <!-- TODO: Fill in the movie overview from the data. -->
-          Movie overivew goes here. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Cras dapibus. Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum.
-        </p>
-        <p class="release-date">Original Release: 1999-12-31</p>
-        <!-- TODO: Fill in the release date from the data. -->
+        <p class="overview">{{result.overview}}</p>
+        <p
+          class="release-date"
+        >Original Release: {{new Date (result.release_date).toLocaleDateString("en-US")}}</p>
+
         <ul class="genre-list">
-          <!-- TODO: Use a loop to iterate through all of the genres for this movie. -->
-          <li>genre</li>
-          <!-- TODO: Fill in the genre name from the data. -->
+          <li v-for="(genre,index) in result.genres" :key="index">{{genre}}</li>
         </ul>
       </li>
     </ul>
